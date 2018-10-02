e3cf2ff17d6ed4b362b8e8e9fb4ecacda617e409
Giovani Ferreira <gioferreira@gmail.com>
2018-10-02 21:47:33 +0000

    Ingest texts and covariates

--- 2 - STM Topic Modelling.R
+++ 2 - STM Topic Modelling.R
@@ -2,3 +2,19 @@
 
 source('src/utils/utils.R')
 library(stm)
+library(tidyverse)
+library(skimr)
+library(lubridate)
+
+posts_tbl_processed <- read_csv("saved_data/posts_tbl_processed_20180930.csv")
+
+posts_txts <- posts_tbl_processed %>%
+  select(id,
+         user_id,
+         first_published_at,
+         text) %>%
+  mutate(first_published_at = floor_date(first_published_at, "days"),
+         day_published = as.integer((first_published_at + days(1)) - min(first_published_at))) %>%
+  select(-first_published_at) %>%
+  arrange_vars(c("day_published" = 3))
+
