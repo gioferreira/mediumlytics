3e9926ddf54e3485205fc90a175168da20eeabae
Giovani Ferreira <gioferreira@gmail.com>
2019-01-31 08:43:57 -0200

    Add PCA, cleanup

--- 2 - TagsWord2Vec.R
+++ 2 - TagsWord2Vec.R
@@ -1,7 +1,8 @@
 # Post 2
 # Word2Vec Tags
 
-# From: https://github.com/h2oai/h2o-3/blob/master/h2o-r/demos/rdemo.word2vec.craigslistjobtitles.R
+######################################################################################
+# Loading Thigs
 
 source('src/utils/utils.R')
 
@@ -21,22 +22,27 @@ palheta <- c("#a0e85b", "#d725a3", "#36e515", "#7f2157", "#7ee8c0", "#fe1d66", "
 
 posts_tbl_processed <- read_rds("saved_data/posts_tbl_processed_20190101.rds")
 
-posts_tbl_processed %>% skim()
+# Sanity Check
+# posts_tbl_processed %>% skim()
 
+######################################################################################
+# Start h2o
 h2o.init()
 
+# Select tags from dataset, concatenate them in one 'phrase'
 tags <- posts_tbl_processed %>%
-  select(num_range("tag_", 1:5)) %>%
+  select(num_range("tag_", 1:5), id) %>%
   mutate_if(is.factor, as.character) %>%
   replace_na(list(tag_1 = "",
                   tag_2 = "",
                   tag_3 = "",
                   tag_4 = "",
                   tag_5 = "")) %>%
-  unite(tags, sep= " ", remove = TRUE) %>%
+  unite(tags, -id, sep = " ", remove = TRUE) %>%
   mutate(tags = str_squish(tags)) %>%
   as.h2o(destination_frame = "tags")
 
+# Custom tokenize function highly commented
 tokenize <- function(sentences) {
   tokenized <- h2o.tokenize(sentences, " ")
   # convert to lower case
@@ -51,46 +57,99 @@ tokenize <- function(sentences) {
   # tokenized.words[is.na(tokenized.words) || (! tokenized.words %in% STOP_WORDS),]
 }
 
+# Model or Load Model
 words <- h2o.na_omit(tokenize(tags$tags))
 
-w2v.model <- h2o.word2vec(words, vec_size = 300, window_size = 3, epochs = 10000)
+# w2v.model <- h2o.word2vec(words, vec_size = 300, window_size = 3, epochs = 10000)
+# 
+# model_path <- h2o.saveModel(object = w2v.model, path = "saved_data", force = TRUE)
 
-model_path <- h2o.saveModel(object = w2v.model, path = "saved_data", force = TRUE)
-
-load_model_path <- "saved_data/Word2Vec_model_R_1548102206983_3"
+load_model_path <- "saved_data/Word2Vec_model_R_1548112930523_2"
 
 w2v.model <- h2o.loadModel(path = load_model_path)
 
-print(h2o.findSynonyms(w2v.model, "os-dez-mandamentos", count = 5))
+# Sanity Check
+print(h2o.findSynonyms(w2v.model, "rapidinhas", count = 5))
 
 word_embedings <- h2o.toFrame(w2v.model)
 
-tsne_model <- Rtsne(as.matrix(word_embedings[,2:101]),
-                    initial_dims = 300,
-                    perplexity = 24,
-                    theta = .05,
-                    max_iter = 20000*2,
-                    eta = 5)
+######################################################################################
+# # PCA Model
+# 
+# pca_model <- prcomp(as.matrix(word_embedings[,2:301]),
+#                     center = TRUE,
+#                     scale. = TRUE)
+# 
+# plot(pca_model, type = "l")
+# 
+# pca_result <- as_data_frame(pca_model$x[,1:2])
+# words_pca <- bind_cols(as_data_frame(words), pca_result)
+# names(words_pca) <- c("Word", "X", "Y")
+# 
+# 
+# clusters <- hdbscan(pca_result, 
+#                     minPts = 5)
+# 
+# words_pca$clhdb <- clusters$cluster
+# 
+# clusters <-  hclust(dist(scale(pca_result)),
+#                     method = "centroid")
+# 
+# words_pca$clhclust <- factor(cutree(clusters, k = 20))
+# 
+# 
+# words_pca %>%
+#   ggplot(aes(x = X, y = Y, color = as.factor(clhclust))) +
+#   # geom_point(size = .5 ) +
+#   geom_text(aes(label = Word),
+#             position=position_jitter(width = .1,
+#                                      height = .1),
+#             size = 3,
+#             fontface = "bold",
+#             alpha = 1,
+#             show.legend = FALSE) +
+#   theme_tufte() + 
+#   theme(axis.title = element_blank(),
+#         axis.text = element_blank(),
+#         axis.ticks = element_blank(),
+#         panel.background = element_rect(fill = "black")) +
+#   scale_color_manual(values = palheta)
+# 
+# # PCA Model gave bad results, it needed many PCs to explain anything meaningful
+######################################################################################
+# Tsne Model
+# # Model or Load
+# tsne_model <- Rtsne(as.matrix(word_embedings[,2:301]),
+#                     initial_dims = 300,
+#                     perplexity = 22,
+#                     theta = .05,
+#                     max_iter = 20000*2,
+#                     eta = 10)
+# 
+# saveRDS(tsne_model, "saved_data/tsne_model-20190122-a.rds")
 
-saveRDS(tsne_model, "saved_data/tsne_model-20190121-c.rds")
+tsne_model <- read_rds("saved_data/tsne_model-20190122-a.rds")
 
-tsne_model <- read_rds("saved_data/tsne_model-20190121-c.rds")
 
 words <- as.data.frame(word_embedings$Word)
 tsne_result <- as.data.frame(tsne_model$Y)
 words_tsne <- bind_cols(words, tsne_result)  
 names(words_tsne) <- c("Word", "X", "Y")
 
+# Cluster with hdbscan
 clusters <- hdbscan(tsne_result, 
                     minPts = 5)
+
 words_tsne$clhdb <- clusters$cluster
 
+# Cluster with hclust
 clusters <-  hclust(dist(scale(tsne_result)),
-                    method = "ward.D2")
+                    method = "centroid")
 
 words_tsne$clhclust <- factor(cutree(clusters, k = 20))
 
 
+# Plot hclust
 words_tsne %>%
   ggplot(aes(x = X, y = Y, color = as.factor(clhclust))) +
   # geom_point(size = .5 ) +
@@ -107,13 +166,8 @@ words_tsne %>%
         axis.ticks = element_blank(),
         panel.background = element_rect(fill = "black")) +
   scale_color_manual(values = palheta)
-  
 
-
-
-
-
-# scale_color_brewer(type = "qual", palette = "Paired")
-
-
-
+######################################################################################
+# Things that helped
+# https://github.com/h2oai/h2o-3/blob/master/h2o-r/demos/rdemo.word2vec.craigslistjobtitles.R
+# https://www.r-bloggers.com/playing-with-dimensions-from-clustering-pca-t-sne-to-carl-sagan/
